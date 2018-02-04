0730a1f5d7fddea47a27f031d9c6963a379246b3
jolene2014 <willyf7@hotmail.com>
2018-02-03 23:57:10 -0600

    PROBANDO LA HERRAMIENTA (Haciendo un commit)

---
+++ TEST1GIT/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SalesOfJanuary ventas = new SalesOfJanuary();
+	
+		displaid();
+		ventas.diplay();
+	}
+		
+		private static void displaid() {
+			
+			System.out.println("FELICES VENTAS DE ENERO");
+			System.out.println("Esta aplicacion muestra informacion");
+		}
+		
+		
+	}
+
+
---
+++ TEST1GIT/src/SalesOfJanuary.java
@@ -0,0 +1,11 @@
+
+public class SalesOfJanuary {
+
+	int data [] = {0,43,55};
+	
+	public void diplay() {
+		
+		System.out.println("Los datos de la implementacios estan mostrados");
+	}
+	
+}
